aca620ed68af5a49d17d1c4f43bb89f25ab1d97f
Agustin Carbajal <magustincarbajal97@gmail.com>
2024-06-08 16:30:33 -0300

    [feature] modifiers

--- app/src/main/java/com/example/learningkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learningkotlin/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -11,22 +13,49 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learningkotlin.ui.theme.LearningkotlinTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { // es todo lo que se va a visualizar dentro de la app
-               Content()
+            Content()
         }
     }
 }
 
+/*
+
+Los modificadores nos permiten moldear o personalizar los componentes
+
+4 tipos de modificadores:
+- De posicionamiento: width, height
+- De funcionalidad:  por ej de click o de scroll
+- De apariencia: background, padding, border
+- De escucha: Cuando se dispara un evento, por ejemplo onKeyEvent
+
+*/
+
 @Preview(showBackground = true)
 @Composable
 fun Content() {
-    Text(text = "Este es el contenido")
+    Text(
+        text = "Este es el contenido",
+        color = Color.Red,
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.sp,
+        modifier = Modifier
+            .background(Color.Black)
+            .padding(horizontal = 30.dp, vertical = 5.dp)
+            .clickable {
+                println("Este es el contenido")
+            }
+    )
 }
 
 
@@ -39,13 +68,13 @@ fun Greeting(phrase: String, modifier: Modifier = Modifier) {
     }
 }
 
-fun hola(){
+fun hola() {
     println("Hola desde jetpack compose")
 }
 
 @Preview
 @Composable
-fun Boton(){
+fun Boton() {
     Button(onClick = {
         println("Hola como estas????")
     }) {
